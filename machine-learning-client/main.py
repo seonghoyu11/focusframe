b5ae666514e34e6c79e3df6021762eb0a6a04ecc
seonghoyu11 <seonghoyu11@gmail.com>
2026-04-11 11:25:16 -0400

    minor fix

--- machine-learning-client/main.py
+++ machine-learning-client/main.py
@@ -8,16 +8,15 @@ import pprint
 img_lst = ["img_png"] # put your image file names here, e.g. ["img1.jpg", "img2.png"]
 count = 1
 for img in img_lst:
-    
     img_path = os.path.join(os.getcwd(), img)
     if not os.path.exists(img_path):
         print(f"Error: File '{img_path}' not found in {os.getcwd()}")
     else:
-
+        img_data = cv2.imread(img_path)
         detector = FER(mtcnn=False)
-        detector.detect_emotions(img)
+        detector.detect_emotions(img_data)
 
-        result = detector.top_emotion(img)
+        result = detector.top_emotion(img_data)
         if result:
                 emotion, score = result
                 pprint.pprint(result)
