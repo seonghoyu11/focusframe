12ba8b43b43137eae5fbb894262d4768be6e4f1f
seonghoyu11 <seonghoyu11@gmail.com>
2026-04-11 11:31:24 -0400

    lint fix

--- machine-learning-client/main.py
+++ machine-learning-client/main.py
@@ -1,26 +1,25 @@
-# skeleton file
-
-from fer.fer import FER
-import cv2
 import os
 import pprint
 
-img_lst = ["img_png"] # put your image file names here, e.g. ["img1.jpg", "img2.png"]
-count = 1
+from fer.fer import FER
+import cv2 
+
+img_lst = ["img.png"]  # put your image file names here, e.g. ["img1.jpg", "img2.png"]
+image_count = 1
 for img in img_lst:
     img_path = os.path.join(os.getcwd(), img)
     if not os.path.exists(img_path):
         print(f"Error: File '{img_path}' not found in {os.getcwd()}")
     else:
-        img_data = cv2.imread(img_path)
+        img_data = cv2.imread(img_path) 
         detector = FER(mtcnn=False)
         detector.detect_emotions(img_data)
 
         result = detector.top_emotion(img_data)
         if result:
-                emotion, score = result
-                pprint.pprint(result)
-                print(f"Result {count}: {emotion} with a score of {score}")
+            emotion, score = result
+            pprint.pprint(result)
+            print(f"Result {image_count}: {emotion} with a score of {score}")
         else:
             print("No face detected. Try an image with a clearer face.")
-    count += 1
+    image_count += 1
