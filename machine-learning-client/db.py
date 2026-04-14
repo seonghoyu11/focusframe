4d000c3359867ce02cf13c5f3cec43af4ddbad6d
InooJung <InooArt@MacBook-Pro-6.local>
2026-04-14 17:37:49 -0400

    Fix pylint issues in db helper

--- machine-learning-client/db.py
+++ machine-learning-client/db.py
@@ -1,5 +1,7 @@
+"""MongoDB helper functions for the machine-learning client."""
+
 import os
-from pymongo import MongoClient
+from pymongo import MongoClient  # pylint: disable=import-error
 
 MONGO_URI = os.environ.get("MONGO_URI", "mongodb://mongodb:27017/appdb")
 DB_NAME = "appdb"
@@ -7,11 +9,13 @@ COLLECTION_NAME = "ml_records"
 
 
 def get_collection():
+    """Return the MongoDB collection used by the ML client."""
     client = MongoClient(MONGO_URI)
-    db = client[DB_NAME]
-    return db[COLLECTION_NAME]
+    database = client[DB_NAME]
+    return database[COLLECTION_NAME]
 
 
 def save_record(record):
+    """Insert one analysis record into MongoDB."""
     collection = get_collection()
     collection.insert_one(record)
