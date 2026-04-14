68fbe8d37da579f8fdafc63879bce5da0e017ab3
seonghoyu11 <seonghoyu11@gmail.com>
2026-04-14 14:26:42 -0400

    pylint fix

--- machine-learning-client/main.py
+++ machine-learning-client/main.py
@@ -1,17 +1,19 @@
+"""Module for emotion detection using FER."""
+
 import os
 import pprint
 
-from fer.fer import FER
-import cv2 
+from fer.fer import FER  # pylint: disable=import-error
+import cv2  # pylint: disable=import-error
 
 img_lst = ["img.png"]  # put your image file names here, e.g. ["img1.jpg", "img2.png"]
-image_count = 1
+image_count = 1  # pylint: disable=invalid-name
 for img in img_lst:
     img_path = os.path.join(os.getcwd(), img)
     if not os.path.exists(img_path):
         print(f"Error: File '{img_path}' not found in {os.getcwd()}")
     else:
-        img_data = cv2.imread(img_path) 
+        img_data = cv2.imread(img_path)  # pylint: disable=no-member
         detector = FER(mtcnn=False)
         detector.detect_emotions(img_data)
 
